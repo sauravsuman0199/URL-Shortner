f5cfc57bbf548c5c20c93c80fd952a9afa1093bf
Saurav Suman <sauravsuman0199@gmail.com>
2024-04-04 16:36:53 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
-
+const shortUrl = require('./routes/url')
+const homeRoutes = require('./routes/home')
 const PORT = 1337
 
 app.use(express.json());
-app.use(express.ststic("public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({extended:false}));
+app.use('/urlapi',shortUrl)
+app.use('/',homeRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server in running at port http://localhost:${PORT}`)
 })
+ 
